a707a4404aec8e1026db71c078b19aa493cdef34
Shungo-I <iririn199810710@gmail.com>
2026-02-23 13:33:42 +0900

    refactor: update Storybook configuration to use spread operator for main and preview configs

--- packages/core/.storybook/main.ts
+++ packages/core/.storybook/main.ts
@@ -1,7 +1,9 @@
 import { createMainConfig } from "@nuance-ui/config/storybook/main";
 import path from "path";
 
-export default createMainConfig(__dirname, {
-  stories: ["../src/**/*.stories.@(ts|tsx)"],
-  coreSourcePath: path.resolve(__dirname, "../src"),
-});
+export default {
+  ...createMainConfig(__dirname, {
+    stories: ["../src/**/*.stories.@(ts|tsx)"],
+    coreSourcePath: path.resolve(__dirname, "../src"),
+  }),
+};
--- packages/core/.storybook/preview.tsx
+++ packages/core/.storybook/preview.tsx
@@ -1,4 +1,6 @@
 import { createPreviewConfig } from "@nuance-ui/config/storybook/preview";
 import { neumorphismTheme } from "../src";
 
-export default createPreviewConfig(neumorphismTheme);
+export default {
+  ...createPreviewConfig(neumorphismTheme),
+};
